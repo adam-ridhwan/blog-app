b60858af47e4726cb0cbeb8846f4eb0b75532777
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-09-12 18:26:18 -0400

    fix: variable

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const nextAuthOptions: NextAuthOptions = {
+export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? '',
@@ -10,6 +10,6 @@ export const nextAuthOptions: NextAuthOptions = {
   ],
 };
 
-const handler = NextAuth(nextAuthOptions);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
