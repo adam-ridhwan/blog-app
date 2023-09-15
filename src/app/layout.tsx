dce70908a119a30a6d5797b50cd1e893a91204a8
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-09-15 00:51:02 -0400

    chore: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import AuthProvider from '@/provider/auth-provider';
 import JotaiProvider from '@/provider/jotai-provider';
 import ThemeProvider from '@/provider/theme-provider';
@@ -14,6 +15,18 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Pondero',
   description: 'Blog site for Pondero',
+  keywords: ['Next.js', 'React', 'Tailwind CSS', 'Server Components', 'Radix UI', 'Blog'],
+  authors: [
+    {
+      name: 'Adam Ridhwan',
+      url: 'https://github.com/adam-ridhwan',
+    },
+  ],
+  creator: 'Adam Ridhwan',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
