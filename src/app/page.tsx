10be6dc72d864f62b92988528a251b6f7b357b6e
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-09-21 20:51:53 -0400

    fix: mock posts and mock users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,156 @@
 import { getPosts } from '@/actions/getPosts';
 import { getUsersById } from '@/actions/getUsersById';
-import { Post } from '@/types';
+import { Post, User } from '@/types';
 import { connectToDatabase } from '@/util/connectToDatabase';
+import { generateRandomString } from '@/util/generateRandomString';
 import { ObjectId } from 'mongodb';
 
 import PostList from '@/components/post-list';
 import SideMenu from '@/components/side-menu';
 
+type Blog = {
+  title: string;
+  content: string;
+};
+
+const blogPosts: Blog[] = [
+  {
+    title: 'The Joys of Traveling Solo',
+    content:
+      'Traveling solo can be a life-changing experience. It offers a chance to reflect and grow as an individual. Moreover, it lets you curate your own itinerary without compromise.',
+  },
+  {
+    title: 'The Digital Revolution and Its Impact',
+    content:
+      'The digital revolution has transformed the way we work and communicate. Automation and digitization are reshaping industries, making them more efficient and consumer-friendly.',
+  },
+  {
+    title: 'The Importance of Mental Health',
+    content:
+      'Mental health is just as crucial as physical health. Recognizing and addressing emotional and psychological issues is vital for overall well-being and leading a balanced life.',
+  },
+  {
+    title: 'Harnessing the Power of Renewable Energy',
+    content:
+      'Renewable energy is the future, as the world seeks sustainable solutions to combat climate change. Solar and wind energy are paving the way for a cleaner planet.',
+  },
+  {
+    title: 'The Renaissance of Classical Literature',
+    content:
+      'Classic literature has seen a resurgence in recent years. Works from centuries ago resonate today, proving the timeless nature of profound storytelling.',
+  },
+  {
+    title: 'Exploring Culinary Delights Around the World',
+    content:
+      'Cuisine tells a lot about a culture. Traveling the globe offers unique opportunities to indulge in diverse culinary experiences, each narrating its own story.',
+  },
+  {
+    title: 'Artificial Intelligence: Promise or Peril?',
+    content:
+      'AI is transforming our world at a rapid pace. While it promises efficiency and innovation, ethical considerations must guide its evolution.',
+  },
+  {
+    title: 'Keeping Fit in a Busy World',
+    content:
+      'Balancing work and fitness can be challenging. Incorporating small changes like short workouts or active commutes can make a big difference.',
+  },
+  {
+    title: 'Reconnecting with Nature',
+    content:
+      "In our tech-driven world, it's essential to take breaks and reconnect with nature. It provides mental peace and a fresh perspective on life.",
+  },
+  {
+    title: 'The Magic of Film and Cinema',
+    content:
+      'Films transport audiences to different worlds, allowing them to experience a myriad of emotions. The art of filmmaking has evolved, but its impact remains profound.',
+  },
+  {
+    title: 'Decoding the Universe: A Look at Astrophysics',
+    content:
+      "Astrophysics delves into the mysteries of the universe. From black holes to galaxies, it seeks answers to some of life's most profound questions.",
+  },
+  {
+    title: 'Sustainability and Fashion: A New Era',
+    content:
+      'The fashion industry is undergoing a transformation. Embracing sustainability not only benefits the planet but also leads to innovative designs and materials.',
+  },
+  {
+    title: 'The Role of Music in Society',
+    content:
+      'Music transcends borders and languages. It has the power to heal, inspire, and bring people together, playing an integral role in societal development.',
+  },
+  {
+    title: 'Innovations in Modern Architecture',
+    content:
+      "Architecture reflects society's evolution. Modern designs prioritize sustainability, functionality, and aesthetics, reshaping our urban landscapes.",
+  },
+  {
+    title: 'Diving into the World of Digital Art',
+    content:
+      'Digital art is blurring the lines between reality and imagination. With technology, artists can now create immersive and interactive masterpieces.',
+  },
+  {
+    title: 'Unraveling the Wonders of Space Exploration',
+    content:
+      "Space exploration has provided insights into our universe's vastness. Each mission uncovers more about our place in this expansive cosmos.",
+  },
+  {
+    title: 'Empathy: The Cornerstone of Human Connection',
+    content:
+      'Empathy allows us to understand and connect with others. It is the foundation of trust, compassion, and genuine human interactions.',
+  },
+  {
+    title: 'The Vibrant World of Street Art',
+    content:
+      "Street art is more than just graffiti. It's a form of expression, commentary, and an integral part of urban culture worldwide.",
+  },
+  {
+    title: 'Preserving Biodiversity: Why Every Species Matters',
+    content:
+      'Biodiversity is essential for ecosystem balance and health. Protecting every species, no matter how small, ensures a thriving and resilient environment.',
+  },
+  {
+    title: 'Unlocking the Power of Mindfulness and Meditation',
+    content:
+      "Mindfulness and meditation are tools for inner peace and clarity. They help navigate life's challenges with grace and poise.",
+  },
+];
+
+const names: string[] = [
+  'Ava Thompson',
+  'James Anderson',
+  'Sophia White',
+  'Benjamin Martinez',
+  'Mia Lewis',
+  'Ethan Walker',
+  'Emily Rodriguez',
+  'William Perez',
+  'Olivia Torres',
+  'Michael Jenkins',
+  'Emma Evans',
+  'Jacob Sanchez',
+  'Amelia Simmons',
+  'Lucas Rivera',
+  'Chloe Hayes',
+  'Jackson James',
+  'Grace Wright',
+  'Elijah Cox',
+  'Abigail Collins',
+  'Alexander Foster',
+];
+
 const generatePost = (index: number): Post => {
-  const title = `${index}`;
+  const title = blogPosts[index].title;
   const postSlug = `mock-post-${index}`;
-  const category = new ObjectId('6505ecac822e8ec0e9b8a1be');
-  const author = new ObjectId('650b3f0649c072e2777bfcd5');
+  const category = new ObjectId('650ce47033901fc25b0af02f');
+  const content = blogPosts[index].content;
+  const author = new ObjectId('650ce4a333901fc25b0af15b');
 
   return {
     createdAt: new Date(),
     postSlug,
     title,
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    content,
     views: Math.floor(Math.random() * 2000),
     categorySlug: 'mock-category',
     category,
@@ -28,13 +160,30 @@ const generatePost = (index: number): Post => {
   };
 };
 
+const generateMockUsers = (index: number): User => {
+  return {
+    name: names[index],
+    username: '@' + `${names[index].split(' ')[0].toLowerCase()}` + '_' + generateRandomString(),
+    email: `${names[index].split(' ')[0].toLowerCase()}@gmail.com`,
+    accounts: [],
+    comments: [],
+    posts: [],
+    sessions: [],
+  };
+};
+
 export default async function Home() {
-  const { postCollection } = await connectToDatabase();
-  const mockPosts = Array.from({ length: 20 }, (_, index) => generatePost(index + 1));
+  const { postCollection, userCollection } = await connectToDatabase();
+  const mockPosts = Array.from({ length: 20 }, (_, index) => generatePost(index));
   // await postCollection.deleteMany({});
   // await postCollection.insertMany(mockPosts);
 
+  const mockUsers = Array.from({ length: 20 }, (_, index) => generateMockUsers(index));
+  // await userCollection.deleteMany({});
+  // await userCollection.insertMany(mockUsers);
+
   const [initialPosts] = await getPosts(5, undefined);
+  console.log(initialPosts);
 
   if (!initialPosts) throw new Error('Failed to fetch initial posts');
 
@@ -42,7 +191,7 @@ export default async function Home() {
   const initialAuthors = await getUsersById(authorIds);
 
   if (!initialAuthors) throw new Error('Failed to fetch initial authors');
-  if (!initialPosts && !initialAuthors) return <div>Please refresh page</div>;
+  if (!initialPosts && !initialAuthors) throw new Error('Failed to fetch initial posts and authors');
 
   const seenAuthors = new Set();
   const uniqueAuthors = initialAuthors.filter(author => {
