9b7a3e4ff50d0a6dd02c875dd736e3a2ff0e02d8
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-10-02 11:33:39 -0400

    style: notFound() desktop style

--- src/app/[username]/[postId]/not-found.tsx
+++ src/app/[username]/[postId]/not-found.tsx
@@ -6,11 +6,11 @@ const NotFound = () => {
   return (
     <>
       <div className='container flex  w-full flex-col items-center justify-center gap-5 pt-[100px] text-center text-primary'>
-        <h1 className='text-4xl font-semibold text-primary'>404</h1>
-        <span className='text-md text-muted'>
+        <h1 className='text-4xl font-semibold text-primary md:text-7xl'>404</h1>
+        <span className='text-md text-muted md:text-3xl'>
           Sorry the page you are looking for does not exist or has been moved.
         </span>
-        <Button variant='accent'>
+        <Button variant='accent' className='md:h-12 md:text-2xl'>
           <Link href='/'>Go back to homepage</Link>
         </Button>
       </div>
