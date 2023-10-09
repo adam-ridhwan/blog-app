4fd193b020ca9db4eddb811d3a228537299aaab6
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-10-09 15:48:40 -0400

    feature: saved posts

--- src/components/side-menu/saved-posts.tsx
+++ src/components/side-menu/saved-posts.tsx
@@ -19,72 +19,46 @@ type SavedPostsProps = {
 };
 
 const SavedPosts: FC<SavedPostsProps> = ({ savedPosts }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   // TODO: Display saved posts instead of trending posts
   return (
     <>
-      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-        <Card className='gap-0'>
-          <CardHeader>
-            <CardTitle className='mb-4'>Saved posts</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className='flex flex-col gap-3 md:grid md:grid-cols-2 lg:flex lg:flex-col'>
-              {Array.from(savedPosts.slice(0, 3)).map(post => (
-                <Link key={post.title} href={`${post.username}/${post._id}`} className='flex items-center'>
-                  <div className='flex flex-col'>
-                    <div className='flex flex-row items-center gap-2'>
-                      <Avatar className='h-5 w-5'>
-                        {/*{image ? <Image src={image} alt='' /> : <AvatarFallback>{name?.split('')[0]}</AvatarFallback>}*/}
-                        {<AvatarFallback className='text-[12px]'>{post.name?.split('')[0]}</AvatarFallback>}
-                      </Avatar>
-                      <span className='font-medium text-muted'>{post.name}</span>
-                    </div>
-                    <h3 className='text-muted-foreground overflow-hidden font-semibold'>{post.title}</h3>
-                    <span className='text-muted'>
-                      {formatDate(post.createdAt.toString()).split(',').splice(0, 1)}
-                    </span>
+      <Card className='gap-4'>
+        <CardHeader>
+          <CardTitle>Saved posts</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-col gap-3 md:grid md:grid-cols-2 lg:flex lg:flex-col'>
+            {Array.from(savedPosts.slice(0, 3)).map(post => (
+              <Link key={post.title} href={`${post.username}/${post._id}`} className='flex items-center'>
+                <div className='flex flex-col'>
+                  <div className='flex flex-row items-center gap-2'>
+                    <Avatar className='h-5 w-5'>
+                      {/*{image ? <Image src={image} alt='' /> : <AvatarFallback>{name?.split('')[0]}</AvatarFallback>}*/}
+                      {<AvatarFallback className='text-[12px]'>{post.name?.split('')[0]}</AvatarFallback>}
+                    </Avatar>
+                    <span className='font-medium text-muted'>{post.name}</span>
                   </div>
-                </Link>
-              ))}
+                  <h3 className='text-muted-foreground overflow-hidden font-semibold'>{post.title}</h3>
+                  <span className='text-muted'>
+                    {formatDate(post.createdAt.toString()).split(',').splice(0, 1)}
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </CardContent>
 
-              <CollapsibleContent className='flex flex-col gap-3 md:grid md:grid-cols-2 lg:flex lg:flex-col'>
-                {Array.from(savedPosts.slice(3, 6)).map(post => (
-                  <Link key={post.title} href={`${post.username}/${post._id}`} className='flex items-center'>
-                    <div className='flex flex-col'>
-                      <div className='flex flex-row items-center gap-2'>
-                        <Avatar className='h-5 w-5'>
-                          {/*{image ? <Image src={image} alt='' /> : <AvatarFallback>{name?.split('')[0]}</AvatarFallback>}*/}
-                          {<AvatarFallback className='text-[12px]'>{post.name?.split('')[0]}</AvatarFallback>}
-                        </Avatar>
-                        <span className='font-medium text-muted'>{post.name}</span>
-                      </div>
-                      <h3 className='text-muted-foreground overflow-hidden font-semibold'>{post.title}</h3>
-                      <span className='text-muted'>
-                        {formatDate(post.createdAt.toString()).split(',').splice(0, 1)}
-                      </span>
-                    </div>
-                  </Link>
-                ))}
-              </CollapsibleContent>
-            </div>
-          </CardContent>
-
-          <CardFooter className={cn({ hidden: isOpen })}>
-            <CollapsibleTrigger asChild>
-              <Button
-                variant='text'
-                className={cn(
-                  `flex h-[40px] w-full items-center justify-center rounded-full border border-border text-muted`
-                )}
-              >
-                See more
-              </Button>
-            </CollapsibleTrigger>
-          </CardFooter>
-        </Card>
-      </Collapsible>
+        <CardFooter>
+          <Button
+            variant='text'
+            className={cn(
+              `flex h-[40px] w-full items-center justify-center rounded-full border border-border text-muted`
+            )}
+          >
+            See more
+          </Button>
+        </CardFooter>
+      </Card>
     </>
   );
 };
